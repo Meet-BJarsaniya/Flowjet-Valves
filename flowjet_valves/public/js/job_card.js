477704f327c7aee37a68cbd9c6d89d46bff3d1b7
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-05-16 18:15:35 +0530

    Job Card Prompt

--- flowjet_valves/public/js/job_card.js
+++ flowjet_valves/public/js/job_card.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('Job Card', {
-    onload: function (frm) {
+    onload: function(frm) {
         if (frm.doc.docstatus == 0 && !frm.doc.custom_job_type) {
             frappe.prompt([
                 {
@@ -11,6 +11,7 @@ frappe.ui.form.on('Job Card', {
                 }
             ], function(values) {
                 frm.set_value('custom_job_type', values.custom_job_type);
+                frm.save();  // Save the form automatically after setting the value
             }, 'Select Job Type', 'Set');
         }
     }
