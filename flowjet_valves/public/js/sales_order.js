30eaba694745ade554c82fe039568181e15a4d35
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-05-22 18:20:06 +0530

    SO Item Rate changes indicator red

--- flowjet_valves/public/js/sales_order.js
+++ flowjet_valves/public/js/sales_order.js
@@ -10,8 +10,38 @@ frappe.ui.form.on('Sales Order', {
     },
     after_save(frm) {
         if (frm.doc.docstatus == 1) updateManufactureCycle(frm);
+    },
+    refresh(frm) {
+        if (!frm.doc.selling_price_list) return;
+
+        frm.doc.items.forEach(row => {
+            if (!row.item_code) return;
+
+            frappe.call({
+                method: "frappe.client.get_value",
+                args: {
+                    doctype: "Item Price",
+                    filters: {
+                        item_code: row.item_code,
+                        price_list: frm.doc.selling_price_list
+                    },
+                    fieldname: "price_list_rate"
+                },
+                callback: function (r) {
+                    if (!r.message) return;
+
+                    let price_list_rate = r.message.price_list_rate;
+                    if (flt(row.rate) !== flt(price_list_rate)) {
+                        $(`[data-idx="${row.idx}"] [data-fieldname="rate"]`).css("color", "red");
+                    } else {
+                        $(`[data-idx="${row.idx}"] [data-fieldname="rate"]`).css("color", "");
+                    }
+                }
+            });
+        });
     }
 });
+
 
 frappe.ui.form.on('Sales Order Item', {
     rate(frm, cdt, cdn) {
@@ -44,6 +74,7 @@ frappe.ui.form.on('Sales Order Item', {
         });
     }
 });
+
 
 function updateManufactureCycle(frm) {
     let item_priority_map = {};
