17d8d90f72891d33fe960718ff88d08b678bc4fe
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-05-28 11:38:32 +0530

    JC pop up for sub-contract qty

--- flowjet_valves/public/js/job_card.js
+++ flowjet_valves/public/js/job_card.js
@@ -8,11 +8,30 @@ frappe.ui.form.on('Job Card', {
                     fieldtype: 'Select',
                     options: ['In-house', 'Sub Contract'],
                     reqd: 1
-                }
+                },
+                {
+                    label: 'Qty To Sub-Contract',
+                    fieldname: 'custom_qty_to_subcontract',
+                    fieldtype: 'Float',
+                    depends_on: "eval:doc.custom_job_type === 'Sub Contract'",
+                    description: 'Total Qty To Manufacture: ' + frm.doc.for_quantity,
+                },
             ], function(values) {
+                // Check required condition manually
+                if (values.custom_job_type === 'Sub Contract' && (!values.custom_qty_to_subcontract || 0 > values.custom_qty_to_subcontract || values.custom_qty_to_subcontract > frm.doc.for_quantity)) {
+                    frappe.msgprint(__('Please enter a valid quantity'));
+                    return;
+                }
+
                 frm.set_value('custom_job_type', values.custom_job_type);
-                frm.save();  // Save the form automatically after setting the value
+
+                if (values.custom_job_type === 'Sub Contract') {
+                    frm.set_value('custom_qty_to_subcontract', values.custom_qty_to_subcontract);
+                    frm.set_value('for_quantity', frm.doc.for_quantity - values.custom_qty_to_subcontract);
+                }
+
+                frm.save(); // Save the form
             }, 'Select Job Type', 'Set');
         }
-    }
+    },
 });
