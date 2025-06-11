19d65875911df732439d64f726841856c8e99657
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-06-11 10:49:28 +0530

    SO tracking page report

--- flowjet_valves/flowjet_valves/page/sales_order_tracking/sales_order_tracking.js
+++ flowjet_valves/flowjet_valves/page/sales_order_tracking/sales_order_tracking.js
@@ -5,6 +5,51 @@ frappe.pages['sales-order-tracking'].on_page_load = function(wrapper) {
 		single_column: true
 	});
 
+    // Define the fields for the filters with default values
+    let fields = [
+        {
+            label: 'From Date',
+            fieldtype: 'Date',
+            fieldname: 'from',
+            default: frappe.datetime.nowdate() // Default to current date
+        },
+        {
+            label: 'To Date',
+            fieldtype: 'Date',
+            fieldname: 'to',
+            default: frappe.datetime.add_months(frappe.datetime.nowdate(), 1) // Default to 1 month after
+        },
+        {
+            label: 'Priority',
+            fieldtype: 'Date',
+            fieldname: 'from',
+            default: frappe.datetime.nowdate() // Default to current date
+        },
+    ];
+
+    let filter_values = {};
+
+    // Create a separate container for the data
+    let $data_container = $('<div id="data-container"></div>').appendTo(page.body);
+
+    // Create filter fields and set the default values
+    fields.forEach(field => {
+        let filterField = page.add_field(field);
+        // Set the initial value to the default specified in the field definition
+        filterField.set_input(field.default);
+        filter_values[field.fieldname] = field.default;
+
+        // Listen for changes in the filter values
+        filterField.$input.on('change', function() {
+            filter_values[field.fieldname] = filterField.get_value();
+            if (filter_values['from'] && filter_values['to']) {
+                fetch_and_render_data(filter_values['from'], filter_values['to']);
+            }
+        });
+    });
+
+    // Function to fetch and render data based on the filters
+    function fetch_and_render_data(from_date, to_date) {
 	let sales_data = [];
 
 	frappe.call({
