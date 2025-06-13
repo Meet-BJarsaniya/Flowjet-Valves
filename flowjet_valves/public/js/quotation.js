af73154766232a81fc5a26ae88c92e95dbf6e78e
Meet-BJarsaniya <meet@sanskartechnolab.com>
2025-06-13 14:38:04 +0530

    quotation to rfq

--- flowjet_valves/public/js/quotation.js
+++ flowjet_valves/public/js/quotation.js
@@ -1,3 +1,25 @@
+frappe.ui.form.on('Quotation', {
+    refresh(frm) {
+        if (frm.doc.docstatus === 0 && !frm.is_new()) {
+            frm.add_custom_button('Request for Quotation', () => {
+                frappe.call({
+                    method: 'flowjet_valves.public.py.quotation.make_rfq_from_quotation',
+                    args: { quotation: frm.doc.name},
+                    callback: function(r) {
+                        if (r.message) {
+                            frappe.model.sync(r.message);
+                            frappe.set_route("Form", r.message.doctype, r.message.name);
+                        } else {
+                            frappe.msgprint("No items found for RFQ.");
+                        }
+                    }
+                });
+            }, 'Create');
+        }
+    }
+});
+
+
 frappe.ui.form.on('Quotation Item', {
     item_code(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
--- flowjet_valves/public/py/quotation.py
+++ flowjet_valves/public/py/quotation.py
@@ -1,5 +1,6 @@
-# import frappe
-# from frappe import _
+import frappe
+from frappe import _
+from frappe.model.mapper import get_mapped_doc
 
 # @frappe.whitelist()
 # def get_last_item_rate(party_name, item_code):
@@ -22,3 +23,48 @@
 #     }, fields=["base_rate"], order_by="creation desc", limit=1)
 
 #     return item[0]["base_rate"] if item else None
+
+
+# Make RFQ from Quotation
+@frappe.whitelist()
+def make_rfq_from_quotation(quotation):
+    # Check for existing Draft RFQ to avoid duplication
+    existing_rfq = frappe.db.exists(
+        "Request for Quotation",
+        {
+            "custom_quotation": quotation,
+            "docstatus": ["in", [0, 1]],  # Draft
+            # "docstatus": 0  # Draft
+        }
+    )
+
+    if existing_rfq:
+        rfq_link = f'<a href="/app/request-for-quotation/{existing_rfq}" target="_blank">{existing_rfq}</a>'
+        frappe.throw(f'Request for Quotation already exists — {rfq_link}', title='Duplicate RFQ')
+
+    rfq = get_mapped_doc(
+        "Quotation",
+        quotation,
+        {
+            "Quotation": {
+                "doctype": "Request for Quotation",
+                "field_map": {
+                    # "transaction_date": "transaction_date",
+                    "company": "company",
+                    "schedule_date": "schedule_date"
+                },
+                # "postprocess": lambda source_doc, target_doc: setattr(target_doc, "quotation", source_doc.name)
+            },
+            "Quotation Item": {
+                "doctype": "Request for Quotation Item",
+                "field_map": {
+                    "item_code": "item_code",
+                    "qty": "qty",
+                    "uom": "uom"
+                }
+            }
+        },
+        target_doc=None
+    )
+    return rfq
+
